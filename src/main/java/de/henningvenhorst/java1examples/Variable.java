8e8d1c64ae5872dc8aa66a71e9ae6ccc0bf6207e
HenningCV <henningvenhorst@gmail.com>
2021-01-04 13:35:57 +0100

    Mein erstes Java Programm :)

---
+++ src/main/java/de/henningvenhorst/java1examples/Variable.java
@@ -0,0 +1,14 @@
+package de.henningvenhorst.java1examples;
+
+public class Variable {
+
+    private String aenderMich ="Ich bin unveraendert!";
+
+    public String getAenderMich() {
+        return aenderMich;
+    }
+
+    public void setAenderMich(String aenderMich) {
+        this.aenderMich = aenderMich;
+    }
+}
---
+++ src/test/java/de/henningvenhorst/java1examples/MyFirstStartClassTest.java
@@ -0,0 +1,12 @@
+package de.henningvenhorst.java1examples;
+
+class MyFirstStartClassTest {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
